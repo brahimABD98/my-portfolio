91bfc226470153668e2b7f63c7a62b000e4f5237
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-12-26 22:14:36 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Brahim Personal Website';
+export const SITE_DESCRIPTION = 'Personal Website with Project Section, CV Section, Paginated Blog, SEO Friendly and Responsive Desing.';
