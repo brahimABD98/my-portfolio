3c14965f390270756633dcf6b7fcfa7e2c86eb32
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-12-26 22:34:22 +0100

    hot fix of rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brahimabd.tech',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
 });
