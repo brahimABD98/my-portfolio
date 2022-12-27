85247f682856c1f21d418cf51292230383c296a5
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-12-27 13:49:53 +0100

    added astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import image from '@astrojs/image';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brahimabd.tech',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+    cacheDir: "./.cache/image"
+  })]
+  
 });
