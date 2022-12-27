91e430d2477f3bb3372f26413c5e3e6c9934d9dc
brahimABD98 <brahim.abderrahim@esprit.tn>
2022-12-27 16:36:14 +0100

    Revert "added astro image"
    This reverts commit 85247f682856c1f21d418cf51292230383c296a5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import image from '@astrojs/image';
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brahimabd.tech',
-  integrations: [mdx(), sitemap(), tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-    cacheDir: "./.cache/image"
-  })]
-  
+  integrations: [mdx(), sitemap(), tailwind()],
 });
