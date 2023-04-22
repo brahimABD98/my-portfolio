bff4d799915345237de105e23178bcb682837f7f
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-04-22 17:21:44 +0100

    added vercel Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brahimabd.tech/',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
-      cacheDir: "./.cache/image",
-      logLevel: 'debug'
-    }
-  )]
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+    cacheDir: "./.cache/image",
+    logLevel: 'debug'
+  }), partytown()]
 });
